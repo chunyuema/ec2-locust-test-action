2f9727e95ed79ba9610ef50d18cb53cf93e452de
Chunyue Ma <cma01@wesleyan.edu>
2022-04-28 17:01:28 -0400

    add slack webhook variable

--- locust/main.tf
+++ locust/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "slack_webhook_url" {
+  type        = string
+  default     = "https://hooks.slack.com/services/SomeSecretValue/SomeSecretValue/SomeSecretValue"
+  description = "To dispatch messages to Slack with the webhook URL."
+}
+
 resource "aws_instance" "wolf_load_test_server_dev" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
