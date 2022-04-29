1bb82c003a4ff3f2443222208a70a780497cdd0a
Chunyue Ma <cma01@wesleyan.edu>
2022-04-28 20:40:41 -0400

    add s flag to curl command

--- locust/locust_test.sh
+++ locust/locust_test.sh
@@ -3,12 +3,12 @@
 # get the instance id
 declare -r instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Test Begins"}' ${slack_webhook_url}
+curl -s -X POST -H 'Content-type: application/json' --data '{"text":"Test Begins"}' ${slack_webhook_url}
 
 # send to slack
 send_to_slack() {
     message=$1
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"test"}' ${slack_webhook_url}
+    curl -s -X POST -H 'Content-type: application/json' --data '{"text":"$message"}' ${slack_webhook_url}
 }
 
 # prepare environment
@@ -39,6 +39,6 @@ else
   send_to_slack "Installing dependencies failed."
 fi
 
-shutdown -h now
-send_to_slack "Scheduled to be terminated."
+# shutdown -h now
+# send_to_slack "Scheduled to be terminated."
 
