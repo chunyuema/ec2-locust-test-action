ae56a99028d966b957a4c8fc0645210109ca8207
Chunyue Ma <cma01@wesleyan.edu>
2022-05-01 12:37:02 -0400

    add data source

--- locust/main.tf
+++ locust/main.tf
@@ -8,9 +8,16 @@ variable "slack_webhook_url" {
   description = "To dispatch messages to Slack with the webhook URL."
 }
 
+data "template_file" "userdata" {
+  template = file("./locust_test.sh")
+  vars = {
+    slack_webhook_url = var.slack_webhook_url
+  }
+}
+
 resource "aws_instance" "wolf_load_test_server_dev" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   tags          = { Name = "Wolf Load Test Dev" }
-  user_data     = file("./locust_test.sh")
+  user_data     = data.template_file.userdata.rendered
 }
