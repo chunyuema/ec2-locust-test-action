786d35b6ac3eba4d7ab4ea558a46a4d5da4382e2
Chunyue Ma <cma01@wesleyan.edu>
2022-05-02 14:30:42 -0400

    add in variable for host endpoint

--- locust/variables.tf
+++ locust/variables.tf
@@ -3,3 +3,9 @@ variable "slack_webhook_url" {
   default     = "https://hooks.slack.com/services/SomeSecretValue/SomeSecretValue/SomeSecretValue"
   description = "To dispatch messages to Slack with the webhook URL."
 }
+
+variable "host_endpoint" {
+  type        = string
+  default     = "https://dummyurl.wolf.jina.ai"
+  description = "Endpoint used to perform the load testing."
+}
