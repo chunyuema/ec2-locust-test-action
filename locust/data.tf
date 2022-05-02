1d1bfe1d792ad99feb1846e8bb05d2c28ee06f99
Chunyue Ma <cma01@wesleyan.edu>
2022-05-02 09:50:37 -0400

    change var for webhook

--- locust/data.tf
+++ locust/data.tf
@@ -1,6 +1,6 @@
 data "template_file" "userdata" {
   template = file("./locust_test.sh")
   vars = {
-    slack_webhook_url = "{$var.slack_webhook_url}"
+    slack_webhook_url = "${var.slack_webhook_url}"
   }
 }
