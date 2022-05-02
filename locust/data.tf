25968e6f13d2abf12f66aeeb43f3fed93d63f83b
Chunyue Ma <cma01@wesleyan.edu>
2022-05-02 14:33:06 -0400

    add host variable

--- locust/data.tf
+++ locust/data.tf
@@ -2,5 +2,6 @@ data "template_file" "userdata" {
   template = file("./locust_test.sh")
   vars = {
     slack_webhook_url = "${var.slack_webhook_url}"
+    host_endpoint     = "${var.host_endpoint}"
   }
 }
