45ef8f571018090a9cf801d76369d180a2caad58
Chunyue Ma <cma01@wesleyan.edu>
2022-05-03 23:43:15 -0400

    temp fix for infra destory

--- locust/main.tf
+++ locust/main.tf
@@ -1,70 +1,70 @@
-resource "aws_security_group" "this" {
-  name        = var.name
-  description = "Allow all traffic"
-  vpc_id      = data.aws_vpc.default.id
+# resource "aws_security_group" "this" {
+#   name        = var.name
+#   description = "Allow all traffic"
+#   vpc_id      = data.aws_vpc.default.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = var.name
-  }
-}
+#   tags = {
+#     Name = var.name
+#   }
+# }
 
-resource "aws_iam_role" "this" {
-  name = var.name
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+# resource "aws_iam_role" "this" {
+#   name = var.name
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_instance_profile" "this" {
-  name = var.name
-  role = aws_iam_role.this.name
-}
+# resource "aws_iam_instance_profile" "this" {
+#   name = var.name
+#   role = aws_iam_role.this.name
+# }
 
-resource "aws_spot_instance_request" "this" {
-  ami                    = var.ami != null ? var.ami : data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  key_name               = var.key_name != "null" ? var.key_name : null
-  subnet_id              = data.aws_subnet.default.id
-  user_data              = data.template_file.userdata.rendered
-  iam_instance_profile   = aws_iam_instance_profile.this.name
-  vpc_security_group_ids = [aws_security_group.this.id]
+# resource "aws_spot_instance_request" "this" {
+#   ami                    = var.ami != null ? var.ami : data.aws_ami.ubuntu.id
+#   instance_type          = var.instance_type
+#   key_name               = var.key_name != "null" ? var.key_name : null
+#   subnet_id              = data.aws_subnet.default.id
+#   user_data              = data.template_file.userdata.rendered
+#   iam_instance_profile   = aws_iam_instance_profile.this.name
+#   vpc_security_group_ids = [aws_security_group.this.id]
 
-  root_block_device {
-    volume_type = var.volume_type
-    volume_size = var.volume_size
-  }
+#   root_block_device {
+#     volume_type = var.volume_type
+#     volume_size = var.volume_size
+#   }
 
-  valid_until                          = timeadd(local.timestamp, "1h")
-  wait_for_fulfillment                 = var.wait_for_fulfillment
-  spot_type                            = var.spot_type
-  instance_interruption_behavior       = "terminate"
-  instance_initiated_shutdown_behavior = "terminate"
+#   valid_until                          = timeadd(local.timestamp, "1h")
+#   wait_for_fulfillment                 = var.wait_for_fulfillment
+#   spot_type                            = var.spot_type
+#   instance_interruption_behavior       = "terminate"
+#   instance_initiated_shutdown_behavior = "terminate"
 
-  tags = {
-    Name = "Wolf Load Test Dev"
-  }
-}
+#   tags = {
+#     Name = "Wolf Load Test Dev"
+#   }
+# }
